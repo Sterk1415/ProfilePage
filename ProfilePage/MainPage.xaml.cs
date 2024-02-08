9c5fb451239d532fde33b6edfb725b605ed1d42e
altus <altus@ALTUS-LEGION>
2024-02-08 20:32:32 +0200

    Add project files.

---
+++ ProfilePage/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ProfilePage
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs args)
+        {
+            await DisplayAlert("Attention", "This part is still under contruction (˶╥︿╥)", "ok");
+        }
+    }
+     
+}
